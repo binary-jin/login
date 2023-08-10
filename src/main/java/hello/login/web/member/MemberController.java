2e98d748c01aac5ee043e4f0d13198878cbb8120
yi-jin <dlwls4338@naver.com>
2023-08-10 15:47:06 +0900

    회원가입, 로그인 로직 완성

--- src/main/java/hello/login/web/member/MemberController.java
+++ src/main/java/hello/login/web/member/MemberController.java
@@ -5,6 +5,7 @@ import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,9 @@ import javax.validation.Valid;
 @RequestMapping("/members")
 public class MemberController {
 
-    private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository; //의존관계 주입
 
+    @GetMapping("/add")
     public String addForm(@ModelAttribute("member") Member member) {
         return "members/addMemberForm";
     }
