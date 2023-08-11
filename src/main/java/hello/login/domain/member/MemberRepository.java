8268f060bd967a0182fb8a746c1f4bd51bce3652
yi-jin <dlwls4338@naver.com>
2023-08-11 21:15:15 +0900

    회원가입, 로그인 로직 수정

--- src/main/java/hello/login/domain/member/MemberRepository.java
+++ src/main/java/hello/login/domain/member/MemberRepository.java
@@ -31,6 +31,7 @@ public class MemberRepository {
 //                return Optional.of(m);
 //            }
 //        }
+
 //        return Optional.empty();
         //Optional은 Optional이라는 통이 있고 그 안에 member 객체를 넣어랴둘 수도 있고 안 넣을 수도 있음
         // -> null로 반환 될 수 있는 경우에 null을 반환하기보다 Optional을 사용함
