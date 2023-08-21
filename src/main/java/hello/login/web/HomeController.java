db55e97b19e303218200716254eee2ce58d4be2c
yi-jin <dlwls4338@naver.com>
2023-08-21 14:39:16 +0900

    쿠키 유지, 해제하기

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,41 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        //require=false는 로그인 하지 않은 사용자도 메인 화면에 들어오게 하기 위해 넣어줘야함
+
+        if(memberId == null) {
+            return "home";
+        }
+
+        //로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
